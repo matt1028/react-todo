3937f7d8ab750f43058412f645f98ca56c5665d5
riiid <mattpark1028@gmail.com>
2021-04-05 17:34:49 +0900

    Finalized Version of Todo List

--- src/components/TodoHead.js
+++ src/components/TodoHead.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useTodoDispatch, useTodoState } from './TodoContext';
+import { useTodoState } from './TodoContext';
 
 const TodoHeadBlock = styled.div`
     padding-top: 48px;
@@ -29,12 +29,20 @@ const TodoHeadBlock = styled.div`
 
 function TodoHead(){
     const todos = useTodoState();
-    console.log(todos);
+    const undoneTasks = todos.filter(todo => !todo.done);
+
+    const today = new Date();
+    const dateString = today.toLocaleDateString('ko-KR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+    });
+    const dayName = today.toLocaleDateString('ko-KR', {weekday: 'long'});
     return(
         <TodoHeadBlock>
-            <h1>2021년 4월 5일</h1>
-            <div className="day">수요일</div>
-            <div className="tasks-left">할 일 2개 남음</div>
+            <h1>{dateString}</h1>
+            <div className="day">{dayName}</div>
+            <div className="tasks-left">할 일 {undoneTasks.length}개 남음</div>
         </TodoHeadBlock>
     );
 }
